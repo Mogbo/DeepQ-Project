81ed8832f54b174529031a77825398df91c21d64
Emmanuel Ojuba <mogbojuba@gmail.com>
2020-12-16 16:03:48 -0500

    Update cartpole processing to plot DQN, DDQN, MDQN on the same plot

--- process_cartpole.py
+++ process_cartpole.py
@@ -1,6 +1,7 @@
 import re
 import numpy as np
 import matplotlib.pyplot as plt
+import glob
 
 def process_file(fname):
     rewards = []
@@ -14,24 +15,45 @@ def process_file(fname):
             reward = float( match.group(1))
             rewards.append(reward)
 
-    return rewards
+    return np.array(rewards)
 
-rewards_norm_done = np.array(process_file("norm_done.txt"))
-rewards_not_norm_done = np.array(process_file("not_norm_done.txt"))
+rewards_mdqn = process_file("MDQN_cartpole_52.txt")
+rewards_ddqn = process_file("DDQN_cartpole_52.txt")
+rewards_dqn = process_file("DQN_cartpole_52.txt")
+#rewards_norm_done = np.array(process_file("checkpoints/CartPole-v0/100k_openai/DDQN/log.txt"))
+#rewards_ddqn = np.array(process_file("checkpoints/CartPole-v0/100k_openai/DDQN/log.txt"))
+# rewards_not_norm_done = np.array(process_file("not_norm_done.txt"))
 
-print("Norm:", np.array(rewards_norm_done)[700:].mean())
-print("Not norm:", np.array(rewards_not_norm_done)[700:].mean())
+# print("Norm:", np.array(rewards_norm_done)[700:].mean())
+# print("Not norm:", np.array(rewards_not_norm_done)[700:].mean())
 
-plt.plot(np.arange(1, len(rewards_norm_done) + 1), rewards_norm_done, label="raw")
+#plt.plot(np.arange(1, len(rewards_mdqn) + 1), rewards_mdqn, label="mdqn")
+#plt.plot(np.arange(1, len(rewards_ddqn) + 1), rewards_ddqn, label="ddqn")
+
+running_avg_mdqn = []
+for idx in range(100, len(rewards_mdqn)):
+    running_avg_mdqn.append(rewards_mdqn[idx - 100: idx].mean())
+running_avg_mdqn = np.array(running_avg_mdqn)
+
+running_avg_ddqn = []
+for idx in range(100, len(rewards_ddqn)):
+    running_avg_ddqn.append(rewards_ddqn[idx - 100: idx].mean())
+running_avg_ddqn = np.array(running_avg_ddqn)
+
+running_avg_dqn = []
+for idx in range(100, len(rewards_dqn)):
+    running_avg_dqn.append(rewards_dqn[idx - 100: idx].mean())
+running_avg_dqn = np.array(running_avg_dqn)
+
+plt.plot(np.arange(100, len(rewards_mdqn)), running_avg_mdqn, label="MDQN", color='C0')
+plt.plot(np.arange(100, len(rewards_ddqn)), running_avg_ddqn, label="DDQN", color='C1')
+plt.plot(np.arange(100, len(rewards_dqn)), running_avg_dqn, label="DQN", color='C2')
+
+plt.axhline(running_avg_mdqn.mean(), label="MDQN avg", color='C0', ls = '--')
+plt.axhline(running_avg_ddqn.mean(), label="DDQN avg", color='C1', ls = '--')
+plt.axhline(running_avg_dqn.mean(), label="DQN avg", color='C2', ls = '--')
+
 plt.xlabel("Episode")
-plt.ylabel("Reward")
-
-running_avg = []
-
-for idx in range(100, len(rewards_norm_done)):
-    running_avg.append(rewards_norm_done[idx-100: idx].mean())
-running_avg = np.array(running_avg)
-
-plt.plot(np.arange(100, len(rewards_norm_done)), running_avg, marker=".", label="running_avg(100 steps)")
-plt.axhline(475, label="target", color='g')
+plt.ylabel("Running Average Reward (100 steps)")
+plt.axhline(195, label="target", color='r')
 plt.legend()
